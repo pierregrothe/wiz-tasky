06d78780d31020a885a65dfb9c4779894b801de6
Pierre Grothé <pierre@grothe.ca>
2025-01-30 15:09:50 -0500

    assume role

--- provider.tf
+++ provider.tf
@@ -12,8 +12,4 @@ terraform {
 provider "aws" {
   region                  = var.aws_region
 
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_account_id}:role/github-actions-oidc"
-    session_name = "github-actions"
-  }
 }
