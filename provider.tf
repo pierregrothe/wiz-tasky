762a049b5ea263b7a503aca3ab3071542fd3f3a5
Pierre Grothé <pierre@grothe.ca>
2025-02-04 17:30:35 -0500

    fix

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.10.5"
+  cloud {
+    organization = "wiz-interview-project"
+
+    workspaces {
+      prefix = "wiz-tasky-"
+    }
+  }
+}
 
   required_providers {
     aws = {
