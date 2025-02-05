5e2697bb908d665b8a247b0562746fdbb5d4bbe3
Pierre Grothé <pierre@grothe.ca>
2025-02-05 14:18:30 -0500

    fix

--- main.tf
+++ main.tf
@@ -22,17 +22,14 @@ module "vpc" {
 module "bastion" {
   source = "./modules/bastion"
 
-  # Required inputs for Bastion module
   vpc_id           = module.vpc.vpc_id
   public_subnet_id = element(module.vpc.public_subnets, 0)  // Use the first public subnet
 
   bastion_instance_type = var.bastion_instance_type
   ami_id                = var.bastion_ami_id
-  bastion_key_name      = var.bastion_key_name
   allowed_ssh_ips       = var.allowed_ssh_ips
 
   project_name     = var.project
   environment_name = var.environment
   tags             = local.all_tags
 }
-
